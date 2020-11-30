a4c603137e466e44c6abfdb1783dab20a767f489
Adrian <dbarra@agetic.gob.bo>
2020-11-30 14:27:57 -0400

    Cambios

---
+++ src/layout/AppSidenav.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { NavigationDrawer, List, ListItem } from 'smelte';
+  let showNav = true;
+  let right = false;
+  let persistent = true;
+  let elevation = true;
+  const menu = [{ to: "/about", text: "About" }, { to: "/blog", text: "Blog" }];
+</script>
+
+<style></style>
+
+<div class="AppSidenav">
+  <NavigationDrawer
+    bind:show={showNav}
+    right={right}
+    persistent={persistent}
+    elevation={elevation}>
+    <h6 class="p-6 ml-1 pb-2 text-xs text-gray-900">Menu</h6>
+    <List items={menu}>
+      <span slot="item" let:item class="cursor-pointer">
+        <a href={item.to}>
+          <ListItem
+            selected={false}
+            {...item}
+            dense
+            navigation />
+        </a>
+      </span>
+    </List>
+    <hr />
+  </NavigationDrawer>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,13 +8,5 @@
 <h1>Great success!</h1>
 
 <div class="container max-w-xl items-center flex flex-col">
-  <Image alt="Borat" src="great-success.png" />
-  <h4 class="my-8">HIGH FIVE!</h4>
-  <div class="my-4">
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </div>
-
-  <Button>I'm a button</Button>
-  <Checkbox label="I'm a checkbox" />
-  <Slider label="Me be slider" />
+  Hola
 </div>
